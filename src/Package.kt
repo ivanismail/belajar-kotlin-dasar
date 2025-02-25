710511a2b186c8fb47355421316febbe3647375d
Ivan Ismail <ivan.ismail@misdm.com>
2025-02-25 16:52:18 +0700

    add: package

---
+++ src/Package.kt
@@ -0,0 +1,6 @@
+import com.example.belajar.util.sayHello
+// import com.example.belajar.util.* import semua fun
+
+fun main() {
+    sayHello("Ivan")
+}
---
+++ src/com/example/belajar/util/StringUtil.kt
@@ -0,0 +1,5 @@
+package com.example.belajar.util
+
+fun sayHello(name: String){
+    println("Helo $name")
+}
